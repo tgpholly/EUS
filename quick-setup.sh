e3d5a84756ff498029a0d71c6a010f15036498aa
Holly <onlinegamer748@gmail.com>
2021-08-21 09:42:08 +0100

    Delete quick-setup.sh

--- quick-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/tgpethan/Revolution.git
-cd Revolution
-npm i
-wget https://raw.githubusercontent.com/tgpethan/EUS/master/EUS.js -P modules/
-rm modules/example_request_handler.js
-cp config/config.example.json config/config.json
-npm i connect-busboy randomstring diskusage get-folder-size
